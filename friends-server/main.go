bf06cdceeffbe5662008c600bb3baeb3d4f6b6ed
Jaewoo Chung <jaewoo@Jaewoos-MacBook-Pro.local>
2025-10-24 13:46:04 -0400

    Add basic HTTP server

---
+++ friends-server/main.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
+}
